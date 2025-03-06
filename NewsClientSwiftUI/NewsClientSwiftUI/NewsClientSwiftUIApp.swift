b98b1615799c49de9ab8e6b9dfd2c94bf8a3bdf3
RuslanWorkGit <lulka758@gmail.com>
2025-03-06 13:06:36 +0200

    Create empty project

---
+++ NewsClientSwiftUI/NewsClientSwiftUI/NewsClientSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsClientSwiftUIApp.swift
+//  NewsClientSwiftUI
+//
+//  Created by Ruslan Liulka on 06.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsClientSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
