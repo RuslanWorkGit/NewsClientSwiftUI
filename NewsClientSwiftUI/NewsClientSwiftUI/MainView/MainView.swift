29145f5608126aacc0900b42762af73a36914444
RuslanWorkGit <lulka758@gmail.com>
2025-03-15 15:23:48 +0200

    add bar with category news

--- NewsClientSwiftUI/NewsClientSwiftUI/MainView/MainView.swift
+++ NewsClientSwiftUI/NewsClientSwiftUI/MainView/MainView.swift
@@ -6,27 +6,70 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct MainView: View {
     
     @StateObject private var viewModel = MainViewModel()
     
     var body: some View {
-        List(viewModel.newsRequest?.articles ?? [], id: \.title) { article in
-            
-            HStack {
-                Image("basicNews")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .clipShape(.buttonBorder)
-                Text(article.title)
+        
+        NavigationView {
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: [GridItem(.flexible())], content: {
+                        ForEach(Category.allCases, id: \.self) { category in
+                            
+                            Text(category.rawValue)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        }
+                    })
+                    .frame(height: 50)
+                }
+                .padding(.horizontal)
+                
+                List(viewModel.newsRequest?.articles ?? [], id: \.title) { article in
+                    
+                    HStack {
+                        
+                        if let url = URL(string: article.urlToImage ?? "") {
+                            WebImage(url: URL(string: article.urlToImage ?? ""))
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .clipShape(.buttonBorder)
+                        } else {
+                            Image("basicNews")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .clipShape(.buttonBorder)
+                        }
+                        
+                        
+                        
+        //                Image("basicNews")
+        //                    .resizable()
+        //                    .frame(width: 80, height: 80)
+        //                    .clipShape(.buttonBorder)
+                        Text(article.title)
+                        
+                    }
+                    
+                    
+                }
+                .onAppear {
+                    viewModel.fetch()
+                }
             }
-            
-            
         }
-        .onAppear {
-            viewModel.fetch()
-        }
+        
+        
+        
+        
+        
+
     }
 }
 
